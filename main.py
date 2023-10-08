c6c7fb92506b88c42745f10e4b73e2bfce0ec8d9
adityasirji <146421388+adityasirji@users.noreply.github.com>
2023-10-08 20:29:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -233,14 +233,14 @@ async def account_login(bot: Client, m: Message):
     editable = await m.reply_text("**Enter Title📥**")
     input0: Message = await bot.listen(editable.chat.id)
     raw_text0 = input0.text
+    
+    editable = await editable.edit("**Downloaded By📥**")
+    input0: Message = await bot.listen(editable.chat.id)
+    raw_text9 = input9.text
 
     await m.reply_text("**Enter resolution📥**")
     input2: Message = await bot.listen(editable.chat.id)
     raw_text2 = input2.text
-
-    editable = await editable.edit("**Downloaded By📥**")
-    input0: Message = await bot.listen(editable.chat.id)
-    raw_text9 = input9.text
 
     editable4 = await m.reply_text("Now send the **Thumb url**\nEg : ```https://telegra.ph/file/d9e24878bd4aba05049a1.jpg```\n\nor Send **no**")
     input6 = message = await bot.listen(editable.chat.id)
